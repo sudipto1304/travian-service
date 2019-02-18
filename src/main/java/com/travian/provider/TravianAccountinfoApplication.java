5beb24bc46508ed9c2c5f29e237b9d638ff36ee4
sudipto <sudipto1306@gmail.com>
2019-02-17 21:12:25 -0600

    2

--- src/main/java/com/travian/provider/TravianAccountinfoApplication.java
+++ src/main/java/com/travian/provider/TravianAccountinfoApplication.java
@@ -2,13 +2,36 @@ package com.travian.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class TravianAccountinfoApplication {
+public class TravianAccountinfoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravianAccountinfoApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TravianAccountinfoApplication.class);
+	}
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis( RequestHandlerSelectors.basePackage( "com.travian.task" ) )
+                .paths(PathSelectors.any())
+                .build();
+    }
 
 }
 
